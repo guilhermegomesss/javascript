6a635237c06a423d5dbf57d71dcc61b2ece99f4a
Guilherme Augusto Gomes <146391647+guilhermegomesss@users.noreply.github.com>
2025-08-13 22:39:41 -0300

    Add files via upload

---
+++ 01- fundamentos JavaScrupt/03-estrutura-de-controle.js
@@ -0,0 +1,25 @@
+// if, else if, else
+
+let idade = 14
+
+if (idade <= 14) {
+    console.log("Você é criança")
+}
+else if (idade > 14 && idade <= 18){
+    console.log("Você é Adolescente")
+}
+else if ( idade > 18 && idade < 60) {
+    console.log("Você é Adulto")
+}
+else {
+    console.log("Você é idoso")
+}
+
+
+// switch
+
+
+// for`, `while`, `do...while`
+
+// for...of`, `for...in`
+
